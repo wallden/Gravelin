32c4bf4476ef70170d6ec056994549bc9b65cd73
Andreas Wilcox <UnknownBum@gmail.com>
2017-01-20 22:27:58 +0100

    Implemented javelin throwin, kickass

---
+++ Gravelin/Assets/JavelinPhysics.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class JavelinPhysics : MonoBehaviour
+{
+	private Rigidbody _rigidBody;
+
+	private void Start()
+	{
+		_rigidBody = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		transform.rotation = Quaternion.LookRotation(_rigidBody.velocity);
+	}
+
+	public void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag != "Spear")
+		{
+			_rigidBody.isKinematic = true;
+			GetComponent<Collider>().isTrigger = true;
+			transform.position = collision.contacts[0].point - transform.forward;
+			Destroy(this);
+		}
+	}
+}
---
+++ Gravelin/Assets/JavelinThrow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class JavelinThrow : MonoBehaviour
+{
+	public Transform Camera;
+
+	private GameObject _spearTemplate;
+
+	private void Start()
+	{
+		_spearTemplate = Resources.Load<GameObject>("Spear");
+		if (_spearTemplate == null)
+		{
+			throw new Exception("Can't load spear");
+		}
+		if (Camera == null)
+		{
+			throw new Exception("Camera reference");
+		}
+	}
+
+	private void Update()
+	{
+		if (CrossPlatformInputManager.GetButtonDown("Fire1"))
+		{
+			var spear = Instantiate(_spearTemplate);
+			spear.transform.position = transform.position + transform.right + transform.forward;
+			spear.transform.rotation = transform.rotation*Quaternion.Euler(Camera.transform.eulerAngles.x, 0, 0);
+			var rigidBody = spear.GetComponent<Rigidbody>();
+			rigidBody.AddForce(spear.transform.forward * 1000);
+		}
+	}
+}
