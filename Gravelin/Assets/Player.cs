3a634a2e07c2d3b9863c19633ab8b8b93cdf0596
Henrik Walldén <henrik@wallden.co>
2017-01-20 19:11:07 +0100

    asd

---
+++ Gravelin/Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.Mouse0))
+	    {
+	        RaycastHit hit;
+
+	        if (Physics.Raycast(transform.position, Camera.main.transform.forward, out hit))
+	        {
+	            var hitSurface = hit.transform;
+                Debug.Log(hit.point);
+	        }
+	    }
+	}
+}
