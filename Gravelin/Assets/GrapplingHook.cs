6b9b604dc921aa7bb675499e1a196491e7cad138
Henrik Walldén <henrik@wallden.co>
2017-01-21 15:24:22 +0100

    sdfsdfs

--- Gravelin/Assets/GrapplingHook.cs
+++ Gravelin/Assets/GrapplingHook.cs
@@ -7,14 +7,21 @@ public class GrapplingHook : NetworkBehaviour
 {
 	public Transform Camera;
 
+	private GameObject _hookPointTemplate;
 	private GameObject _hookLineTemplate;
 	private Rigidbody _rigidBody;
 	private bool _grappling;
+	private GameObject _hookPoint;
 	private GameObject _hookLine;
 
 	public void Start()
 	{
-		_hookLineTemplate = Resources.Load<GameObject>("HookLine");
+		_hookPointTemplate = Resources.Load<GameObject>("HookPoint");
+		if (_hookPointTemplate == null)
+		{
+			throw new Exception("Can't load hook point");
+		}
+        _hookLineTemplate = Resources.Load<GameObject>("HookLine");
 		if (_hookLineTemplate == null)
 		{
 			throw new Exception("Can't load hook line");
@@ -47,23 +54,27 @@ public class GrapplingHook : NetworkBehaviour
     private void CmdPerformNewGrapple()
 	{
 		RaycastHit hit;
-
-		if (Physics.Raycast(transform.position, Camera.transform.forward, out hit))
+    	if (Physics.Raycast(transform.position, Camera.transform.forward, out hit))
 		{
-			_hookLine = Instantiate(_hookLineTemplate);
-			_hookLine.transform.position = hit.point;
-			var line = _hookLine.transform.FindChild("Line");
-			line.rotation = Quaternion.LookRotation(transform.position - line.transform.position);
-			line.localScale = new Vector3(line.localScale.x, line.localScale.y, hit.distance);
-			var lineJoint = line.GetComponent<ConfigurableJoint>();
+			_hookPoint = Instantiate(_hookPointTemplate);
+			_hookPoint.transform.position = hit.point;
+            _hookLine = Instantiate(_hookLineTemplate);
+		    _hookLine.transform.position = hit.point;
+            _hookLine.transform.rotation = Quaternion.LookRotation(transform.position - _hookLine.transform.position);
+			_hookLine.transform.localScale = new Vector3(_hookLine.transform.localScale.x, _hookLine.transform.localScale.y, hit.distance);
+		    _hookPoint.GetComponent<ConfigurableJoint>().connectedBody = _hookLine.GetComponent<Rigidbody>();
+            var lineJoint = _hookLine.GetComponent<ConfigurableJoint>();
 			lineJoint.connectedBody = _rigidBody;
 			_grappling = true;
+            NetworkServer.Spawn(_hookPoint);
             NetworkServer.Spawn(_hookLine);
     	}
 	}
     private void CmdReleaseGrapple()
 	{
+
         Destroy(_hookLine);
+        Destroy(_hookPoint);
 		_grappling = false;
 	}
 }
