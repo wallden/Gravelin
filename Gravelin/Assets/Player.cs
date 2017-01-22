e2c3064b71bce8b3a8899a1a3ab7c50e0acc9be2
Henrik Walldén <henrik@wallden.co>
2017-01-22 14:20:41 +0100

    inputs o skit

--- Gravelin/Assets/Player.cs
+++ Gravelin/Assets/Player.cs
@@ -11,13 +11,16 @@ public class Player : MonoBehaviour
     public string playerNumber;
     public float speed;
     public float jumpForce;
+    public float dashForce;
+
     [HideInInspector]
     public bool isAlive = true;
+    [HideInInspector]
+    public bool isAirborn;
 
-    public bool isAirborn = false;
-
+    private double _dashCooldown;
     private MouseOrbitImproved _camera;
-
+    
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -25,8 +28,9 @@ public class Player : MonoBehaviour
         var cam = GetComponentInChildren<Camera>();
         cam.enabled = true;
         GetComponent<MeshRenderer>().material.color = Color.blue;
-        speed = 20f;
+        speed = 1.2f;
         jumpForce = 9f;
+        dashForce = 15f;
     }
 
     // Update is called once per frame
@@ -45,26 +49,42 @@ public class Player : MonoBehaviour
         if (!isAlive)
             return;
 
-        var horizontalValue = CrossPlatformInputManager.GetAxis("MoveHorizontal_" + playerNumber);
-        rigidbody.AddForce(transform.right * (horizontalValue * speed));
+        if (!isAirborn)
+        {
+            var horizontalValue = CrossPlatformInputManager.GetAxis("MoveHorizontal_" + playerNumber);
+            rigidbody.AddForce(transform.right*(horizontalValue*speed), ForceMode.VelocityChange);
 
-        var forwardValue = -CrossPlatformInputManager.GetAxis("MoveVertical_" + playerNumber);
-        rigidbody.AddForce(transform.forward * (forwardValue * speed));
+            var forwardValue = -CrossPlatformInputManager.GetAxis("MoveVertical_" + playerNumber);
+            rigidbody.AddForce(transform.forward*(forwardValue*speed), ForceMode.VelocityChange);
 
+            if (horizontalValue == 0f && forwardValue == 0f)
+            {
+                rigidbody.velocity = Vector3.zero;
+            }
+        }
         if (CrossPlatformInputManager.GetButtonDown("Jump_" + playerNumber))
         {
-             rigidbody.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
-            isAirborn = true;
+            if (!isAirborn)
+            {
+                rigidbody.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+                isAirborn = true;
+            }
         }
-        if (!isAirborn && horizontalValue == 0f && forwardValue == 0f)
+        if (CrossPlatformInputManager.GetButtonDown("Dash_" + playerNumber))
         {
-            rigidbody.velocity = Vector3.zero;
+            if (isAirborn && _dashCooldown == 0f)
+            {
+                rigidbody.AddForce(transform.forward * dashForce, ForceMode.Impulse);
+                _dashCooldown = 1f;
+            }
         }
     }
 
+    
     void OnCollisionEnter(Collision collision)
     {
         isAirborn = false;
+        _dashCooldown = 0f;
     }
     ConfigurableJoint ConfigureJoint(ConfigurableJoint joint)
     {
